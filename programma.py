7dfce254efd2f82eeb093b1be4817b4854d97c88
FireFlashing <filippoettoreg@gmail.com>
2021-09-24 12:05:53 +0200

    Support for audio splitting and settings

--- programma.py
+++ programma.py
@@ -1,15 +1,28 @@
 import speech_recognition
 import os
-from json import load
+from json import load, dumps, dump
 from sys import exit as killini
-from pydub import AudioSegment
+from pydub import AudioSegment 
+from pydub.silence import split_on_silence
 import sys
 from pathlib import Path
+from time import sleep
+from traceback import format_exc
 
 if getattr(sys, 'frozen', False):
     program_path = os.path.dirname(sys.executable)+"/"
 elif __file__:
     program_path = os.path.dirname(os.path.realpath(__file__)).replace("\\","/")+"/"
+
+def jread(relative_path):
+    with open(program_path+relative_path,encoding="utf-8") as fail:
+        data=load(fail)
+    return data
+def jwrite(relative_path,data):
+    with open(program_path+relative_path,"w") as fail:
+        fail.write(dumps(data,indent=4))
+        fail.close()
+settings_dict=jread("settings.json")
 
 dir_path=str(Path(program_path).parent.absolute())+"/"
 
@@ -21,15 +34,66 @@ if not os.path.exists(dir_path):
 if not os.path.exists(program_path+"temp_files/"):
     os.makedirs(program_path+"temp_files/")
 
-recognitore=speech_recognition.Recognizer()
+for f in os.listdir(program_path+"temp_files/"):
+    if os.path.exists(program_path+"temp_files/"+f):
+        os.remove(program_path+"temp_files/"+f)
 
+recognitore=speech_recognition.Recognizer()
 
 loop=True
 while loop:
-    nome_input=input("Inserisci il nome dell'audio (con estensione)\n>")
+    nome_input=input("Inserisci il nome dell'audio (con estensione) oppure premi invio per modificare le impostazioni\n>")
     if os.path.isfile(dir_path+nome_input):
         original_file=nome_input.split(".")
         loop=False
+    elif nome_input=="":
+        modify_target=""
+        while modify_target not in ["0","1","2"]:
+            modify_target=input("Digita il numero corrispondente alla proprietà che desideri modificare\n0=Durata minima del silenzio perché il file audio venga diviso\n1=Intensità del silenzio in dBFS\n2=Stringa posta al termine di un segmento\n>")
+        if modify_target=="0":
+            loopi=True
+            print("I file audio lunghi vengono divisi in sezioni in modo da essere più facili da processare.\nIl valore che si sta modificando equivale a quanti millisecondi deve durare un momento di silenzio perché il programma usi la pausa come divisione.\nIl valore consigliato è di 500; Il valore attuale è di %i.\nDigitare il valore oppure premere invio per mantenere il valore attuale"%(settings_dict["Split silence"]))
+            while loopi:
+                silence_amount=input(">")
+                if silence_amount!="":
+                    try:
+                        silence_amount=int(silence_amount)
+                        settings_dict["Split silence"]=silence_amount
+                        jwrite("settings.json",settings_dict)
+                        loopi=False
+                    except:
+                        pass
+                else:
+                    loopi=False
+        elif modify_target=="1":
+            loopi=True
+            print("I file audio lunghi vengono divisi in sezioni in modo da essere più facili da processare.\nIl valore che si sta modificando equivale al livello di volume in dBFS che viene considerato silenzio.\nIl valore consigliato è di -36; Il valore attuale è di %i.\nDigitare il valore oppure premere invio per mantenere il valore attuale"%(settings_dict["Split audio"]))
+            while loopi:
+                this_input=input(">")
+                if this_input!="":
+                    try:
+                        this_input=int(this_input)
+                        settings_dict["Split audio"]=this_input
+                        jwrite("settings.json",settings_dict)
+                        loopi=False
+                    except:
+                        pass
+                else:
+                    loopi=False
+        elif modify_target=="2":
+            loopi=True
+            print("I file audio lunghi vengono divisi in sezioni in modo da essere più facili da processare.\nIl valore che si sta modificando equivale a ciò che viene scritto nel testo al termine di un segmento.\nVa tenuto presente che \\n significa 'a capo'.\nIl valore consigliato è \\n; Il valore attuale è %s.\nDigitare il valore oppure premere invio per mantenere il valore attuale"%(settings_dict["Section end"].replace("\n","\\n")))
+            while loopi:
+                this_input=input(">")
+                if this_input!="":
+                    try:
+                        settings_dict["Section end"]=this_input.replace("\\n","\n")
+                        jwrite("settings.json",settings_dict)
+                        loopi=False
+                    except:
+                        pass
+                else:
+                    loopi=False
     else:
         print("\tSembra che questo file non esista nella stessa cartella del programma")
 
@@ -53,48 +117,56 @@ if ambient_noise:
 
 nome_output=input("Inserisci il nome del file dove vuoi vengano salvati i testi (l'estensione verrà impostata automaticamnete come .txt)\n>")
 
-different_format=False
 print("──────\n┌Inizio dell'importazione del file audio")
-if original_file[1]!="wav":
-    print("│\t┌Inizio conversione a .wav per lettura corretta")
-    wrong_audio=AudioSegment.from_file(dir_path+nome_input,format=original_file[1])
-    wrong_audio.export(program_path+"temp_files/"+original_file[0]+".wav",format="wav")
-    print("│\t└Fine conversione a .wav")
-    file_audio = speech_recognition.AudioFile(program_path+"temp_files/"+original_file[0]+".wav")
-    different_format=True
-else:
-    file_audio = speech_recognition.AudioFile(dir_path+nome_input)
+file_audio=AudioSegment.from_file(dir_path+nome_input,format=original_file[1])
 print("└Fine dell'importazione del file audio")
 
-with file_audio as source:
-    print("┌Inizio dell'elaborazione del file audio")
-    if ambient_noise:
-        print("│\t┌Inizio riduzione del rumore")
-        recognitore.adjust_for_ambient_noise(source,ambient_noise_duration)
-        print("│\t└Fine riduzione del rumore")
-    audio = recognitore.record(source)
-    print("└Fine dell'elaborazione del file audio")
-print("┌Inizio sintetizzazione del file audio")
-try:
-    testo=recognitore.recognize_google(audio,language="it-IT")
-except:
-    print("│\t┌Inizio riduzione del rumore a seguito di un errore di sintetizzazione")
-    with file_audio as source:
-        recognitore.adjust_for_ambient_noise(source)
-        audio = recognitore.record(source)
-        print("│\t│Secondo tentativo di sintetizzazione")
-        testo=recognitore.recognize_google(audio,language="it-IT")
-        print("│\t└Fine secondo tentativo di sintetizzazione")
-print("└Fine sintetizzazione del file audio")
+print("┌Inizio divisione del file audio a silenzi con parametri di %i per durata e %i per intensità (ci vuole sempre un bel po')"%(settings_dict["Split silence"],settings_dict["Split audio"]))
+chunks_of_audio=split_on_silence(file_audio,min_silence_len=settings_dict["Split silence"],silence_thresh=settings_dict["Split audio"])
+print("└Fine divisione del file audio (%i segmenti ottenuti)"%(len(chunks_of_audio)))
+output_file=open(dir_path+nome_output+".txt","w+")
+i=0
+failed=0
+print("┌Inizio elaborazione dei segmenti")
+for audio_chunk in chunks_of_audio:
+    print("│\t┌Inizio elaborazione del segmento %i/%i-%i"%(i+1,len(chunks_of_audio),int((100*(i+1))/len(chunks_of_audio)))+"%")
 
-print("┌Inizio trascrizione del testo nel file %s.txt"%(nome_output))
-output_file=open(dir_path+nome_output+".txt","w")
-output_file.write(testo)
-output_file.close()
-if different_format:
-    print("│\t┌Inizio rimozione file temporaneo")
-    if os.path.exists(program_path+"temp_files/"+original_file[0]+".wav"):
-        os.remove(program_path+"temp_files/"+original_file[0]+".wav")
-    print("│\t└Fine rimozione file temporaneo")
-input("└Fine trascrizione. Premere invio per uscire dal programma")
-#recognitore.recognize_houndify(source)
+    audio_chunk=AudioSegment.silent(duration=500)+audio_chunk+AudioSegment.silent(duration=500)
+    audio_chunk.export(program_path+"temp_files/chunk%i.wav"%(i),format="wav")
+
+    with speech_recognition.AudioFile(program_path+"temp_files/chunk%i.wav"%(i)) as source:
+        if ambient_noise:
+            recognitore.adjust_for_ambient_noise(source,ambient_noise_duration)
+
+        audio_ascoltato=recognitore.record(source)
+        retry=True
+        ii=0
+        print("│\t│\t┌Inizio trascrizione audio")
+        while retry and ii<5:
+            try:
+                testo=recognitore.recognize_google(audio_ascoltato,language="it-IT")
+                retry=False
+                output_file.write(testo+settings_dict["Section end"])
+            except speech_recognition.UnknownValueError:
+                print("│\t│\t┆Testo non riconosciuto")
+                retry=False
+            except Exception as e:
+                if ii<4:
+                    print("│\t│\t├Errore di connessione. Si ripeterà il tentativo tra 5 secondi.")
+                    sleep(5)
+                ii+=1
+                if ii==5:
+                    if not os.path.exists(dir_path+"failed_files/"):
+                        os.makedirs(dir_path+"failed_files/")
+                    print("│\t│\t├Esportazione del file audio tra i file non trascritti.")
+                    audio_chunk.export(dir_path+"failed_files/%s.%s-chunk-%i.wav"%(original_file[0],original_file[1],i+1),format="wav")
+                    output_file.write("[TRASCRIZIONE FALLITA PER AUDIO %s.%s-chunk-%i.wav]"%(original_file[0],original_file[1],i+1)+settings_dict["Section end"])
+                    failed+=1
+        print("│\t│\t└Fine trascrizione audio")
+        try:
+            os.remove(program_path+"temp_files/chunk%i.wav"%(i))
+        except:
+            pass
+    print("│\t└Fine elaborazione del segmento")
+    i+=1
+input("└Fine elaborazione dei segmenti. Il risultato è stato salvato in %s.txt\nA causa di problemi di connessione o di file troppo estesi, non è stato possibile trascrivere %i file.\nSi possono ascoltare i file non trascritti nella cartella failed_files\nPremere invio per chiudere il programma"%(nome_output,failed))
